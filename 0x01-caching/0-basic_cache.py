d98b98e1bc42c091930ce8d0e12580133650065b
Mars Adeniji <adenijifara@gmail.com>
2025-02-03 00:17:33 +0200

    Task complete

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from base_caching import BaseCaching
+'''Basic dictionary'''
+
+
+class BasicCache(BaseCaching):
+    '''Basic dictionary caching'''
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        '''Add an item in the cache'''
+        if key or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''Get an item by key'''
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
