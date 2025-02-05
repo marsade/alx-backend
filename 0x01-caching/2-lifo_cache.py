304f213e3896a4ea0c3b64a5c2c035f3330a74de
Mars Adeniji <adenijifara@gmail.com>
2025-02-05 15:55:41 +0200

    Task complete

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+'''LIFO Caching'''
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LIFOCache(BaseCaching):
+    '''LIFO Caching Class Implementation'''
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        '''Add an item in the cache'''
+        if key is None and item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            lifo_key, lifo_item = self.cache_data.popitem()
+            print('DISCARD:', lifo_key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        '''Get an item by key'''
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
+
+my_cache = LIFOCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "ALX")
+my_cache.put("D", "School")
+my_cache.print_cache()
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
